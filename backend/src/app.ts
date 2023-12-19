ad4ea6e83bf8f82316406654ac83ee81d30024d6
Dschabrail <141074515+Dschabrail@users.noreply.github.com>
2023-12-19 22:51:12 +0100

    installed morgan and added folders

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import { config } from 'dotenv';
+import morgan from 'morgan'
 config();
 
 
 const app = express();
 app.use(express.json());
+app.use(morgan("dev"));
+app.use("/api/v1");
 
 export default app;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import app from "./app.js";
 import { connectToDatabase } from "./db/connection.js";
 
+const PORT = process.env.PORT || 5000;
+
 connectToDatabase().then(() => {
-  app.listen(5000, () => console.log("server Open"));
+  app.listen(PORT, () => console.log("server Open"));
 }).catch((err) => console.log(err));
 
