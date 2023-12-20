58ab1f16b35bb3af3ee567c3e8f656edfc2a1ed1
Dschabrail <141074515+Dschabrail@users.noreply.github.com>
2023-12-20 16:21:09 +0100

    routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,23 @@
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Chat from "./pages/Chat";
+import Signup from "./pages/Signup";
+
 function App() {
   return (
-   <main>Hello world</main>
+   <main>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/signup" element={<Signup />}/>
+      <Route path="/chat" element={<Chat />}/>
+      <Route path="*" element={<NotFound />}/>
+    </Routes>
+   </main>
   )
 }
 
