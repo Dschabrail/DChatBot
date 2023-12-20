6d849aa369abbea74b8d93ad1bded1e185aa53bf
Dschabrail <141074515+Dschabrail@users.noreply.github.com>
2023-12-20 21:38:57 +0100

    header logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,23 @@ import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import Chat from "./pages/Chat";
 import Signup from "./pages/Signup";
+import { useAuth } from "./context/AuthContext";
 
 function App() {
-  return (
-   <main>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/login" element={<Login />}/>
-      <Route path="/signup" element={<Signup />}/>
-      <Route path="/chat" element={<Chat />}/>
-      <Route path="*" element={<NotFound />}/>
-    </Routes>
-   </main>
-  )
+  console.log(useAuth()?.isLoggedIn);
+
+  return ( 
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -3,35 +3,35 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Logo = () => {
-    return (
-      <div
-        style={{
-          display: "flex",
-          margin: "auto",
-          alignItems: "center",
-          gap: "8px",
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: "8px",
+      }}
+    >
+      <Link to={"/"}>
+        <img
+          src="./src/assets/openai.png"
+          alt="openai"
+          width={"30px"}
+          height={"30px"}
+          className="image-inverted"
+        />
+      </Link>
+      <Typography
+        sx={{
+          display: { md: "block", sm: "none", xs: "none" },
+          mr: "auto",
+          fontweight: "800",
+          textShadow: "2px 2px 20px #000",
         }}
       >
-        <Link to={"/"}>
-          <img
-            src="./src/assets/openai.png"
-            alt="openai"
-            width={"30px"}
-            height={"30px"}
-            className="image-inverted"
-          />
-          <Typography
-            sx={{
-              display: { md: "block", sm: "none", xs: "none" },
-              mr: "auto",
-              fontweight: "800",
-              textShadow: "2px 2px 20px #000",
-            }}>
-              <span style={{ fontSize: "20px"}}>MERN</span>-GPT
-            </Typography>
-        </Link>
-      </div>
-    );
-  };
+        <span style={{ fontSize: "20px" }}>DChatBot</span>
+      </Typography>
+    </div>
+  );
+};
 
-export default Logo
+export default Logo;
