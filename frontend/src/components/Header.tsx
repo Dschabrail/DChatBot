f9659fd9ba5f9193b6408937fb7afc9071fda55b
Dschabrail <141074515+Dschabrail@users.noreply.github.com>
2023-12-21 12:45:18 +0100

    box signup and login

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -9,15 +9,15 @@ const Header = () => {
   return (
     <AppBar
       sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}>
-      <Toolbar sx={{ display: "flex" }}>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
         <Logo />
         <div>
           {auth?.isLoggedIn ? <>
-          <NavigationLink bg="#00fffc" to="/chat" text="Login" textColor="black"/>
-          <NavigationLink bg="#51538f" to="/" text="logout" textColor="white" onClick={auth.logout}/>
+          <NavigationLink bg="#00fffc" to="/chat" text="Go to Chat" textColor="black"/>
+          <NavigationLink bg="#51538f" to="/" text="Logout" textColor="white" onClick={auth.logout}/>
           </> : <>
           <NavigationLink bg="#00fffc" to="/login" text="Signup" textColor="black"/>
-          <NavigationLink bg="#51538f" to="/signup" text="logout" textColor="white"/>
+          <NavigationLink bg="#51538f" to="/signup" text="Login" textColor="white"/>
           </>}
         </div>
       </Toolbar>
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Box } from "@mui/material";
 
-function Login() {
+const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <Box width={"100%"} height={"100%"} display="flex" flex={1}>
+      <Box
+        padding={8}
+        mt={8}
+        display={{ md: "flex", sm: "none", xs: "none" }}
+      >
+        <img src="./src/assets/airobot.png" alt="Robot" style={{width:"400px"}} />
+      </Box>
+      <Box></Box>
+    </Box>
+  );
+};
 
-export default Login
+export default Login;
