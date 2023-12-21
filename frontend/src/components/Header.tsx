f63dd8842ba96e278f2d1dc39356d94115fff45b
Dschabrail <141074515+Dschabrail@users.noreply.github.com>
2023-12-21 11:42:36 +0100

    navigation Link

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,18 @@
 import { AppBar, Toolbar } from "@mui/material";
 import React from "react";
 import Logo from "./shared/Logo";
+import { useAuth } from "../context/AuthContext";
 
 const Header = () => {
+  const auth = useAuth();
   return (
     <AppBar
-      sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
-    >
+      sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}>
       <Toolbar sx={{ display: "flex" }}>
         <Logo />
+        <div>
+          {auth?.isLoggedIn ? <></> : <></>}
+        </div>
       </Toolbar>
     </AppBar>
   );
